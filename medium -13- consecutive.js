57e59e7319d1c5578c7ba18634ee646e28659dfd
tomarak <ankuto@gmail.com>
2014-10-29 12:30:38 -0400

    Create medium -13- consecutive.js

---
+++ medium -13- consecutive.js
@@ -0,0 +1,19 @@
+/*
+Have the function Consecutive(arr) take the array of integers stored in arr and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number. For example: If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Negative numbers may be entered as parameters and no array will have less than 2 elements. 
+*/
+
+function Consecutive(arr){
+
+//for each array value, find the distance or amount of numbers between the array value and the next array value
+//send the amount of numbers to a total value
+//return the total
+
+var total = 0;
+var diff;
+for(var i = 0; i+1 < arr.length; i++){
+	diff = (arr[i+1] - arr[i]) - 1;
+	total+=diff;
+}
+return total;
+
+}
